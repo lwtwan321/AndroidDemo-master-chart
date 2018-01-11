30d430817a54a960690f3c7a4c45ff85ab5be116
apple <lwtwan321@126.com>
2018-01-11 13:33:44 +0800

    1

---
+++ progressview/src/main/java/com/xinshiwi/power/progressview/newweek/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.xinshiwi.power.progressview.newweek;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xinshiwi.power.progressview.R;
+
+/**
+ * Created by apple on 2018/1/10.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
